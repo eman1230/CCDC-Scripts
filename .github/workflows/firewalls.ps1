8a729f17a47fec2962b88781c95608626d6e1d18
Ethan Matz <152338679+eman1230@users.noreply.github.com>
2025-11-20 19:38:05 -0500

    Update firewalls.ps1

--- .github/workflows/firewalls.ps1
+++ .github/workflows/firewalls.ps1
@@ -1,52 +1,54 @@
 # ============================================================
-# CCDC ESSENTIAL WINDOWS SERVICE FIREWALL RULESET
-# Run as Administrator
+# CORRECTED CCDC WINDOWS SERVICE HARDENING SCRIPT
 # ============================================================
 
-# -------- RDP --------
-netsh advfirewall firewall add rule name="RDP" dir=in action=allow protocol=TCP localport=3389
+$DisableServices = @(
+    "DiagTrack",
+    "DoSvc",
+    "DPS",
+    "SysMain",
+    "TabletInputService",
+    "WpnService",
+    "WpnUserService",
+    "TrkWks",
+    "Themes",
+    "TokenBroker",
+    "AppXSvc",
+    "DispBrokerDesktopSvc",
+    "LicenseManager",
+    "MapsBroker",
+    "OneSyncSvc",
+    "MessagingService",
+    "CDPSvc",
+    "CDPUserSvc",
+    "cbdhsvc",
+    "StorSvc",
+    "ShellHWDetection",
+    "PhoneSvc",
+    "Fax",
+    "RetailDemo",
+    "UevAgentService",
+    "SEMgrSvc",
+    "SCardSvr",
+    "XblAuthManager",
+    "XblGameSave",
+    "XboxNetApiSvc",
+    "SharedAccess",
+    "lfsvc",
+    "WbioSrvc",
+    "bthserv",
+    "BluetoothUserService"
+)
 
-# -------- SMB / FILE SHARING --------
-netsh advfirewall firewall add rule name="SMB-137" dir=in action=allow protocol=UDP localport=137
-netsh advfirewall firewall add rule name="SMB-138" dir=in action=allow protocol=UDP localport=138
-netsh advfirewall firewall add rule name="SMB-139" dir=in action=allow protocol=TCP localport=139
-netsh advfirewall firewall add rule name="SMB-445" dir=in action=allow protocol=TCP localport=445
+foreach ($svc in $DisableServices) {
+    Write-Host "Processing service: $svc"
+    
+    # Stop service safely
+    sc.exe stop $svc 2>$null
+    
+    # Disable service (correct syntax with required space)
+    sc.exe config $svc start= disabled 2>$null
+}
 
-# -------- DNS (TCP/UDP 53) --------
-netsh advfirewall firewall add rule name="DNS-TCP" dir=in action=allow protocol=TCP localport=53
-netsh advfirewall firewall add rule name="DNS-UDP" dir=in action=allow protocol=UDP localport=53
+Write-Host "Finished disabling unnecessary services."
 
-# -------- WEB SERVER (HTTP/HTTPS) --------
-netsh advfirewall firewall add rule name="HTTP" dir=in action=allow protocol=TCP localport=80
-netsh advfirewall firewall add rule name="HTTPS" dir=in action=allow protocol=TCP localport=443
-
-# -------- WinRM (Remote PowerShell) --------
-netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in action=allow protocol=TCP localport=5985
-netsh advfirewall firewall add rule name="WinRM-HTTPS" dir=in action=allow protocol=TCP localport=5986
-
-# -------- WMI (RPC Endpoint) --------
-netsh advfirewall firewall add rule name="WMI-RPC" dir=in action=allow protocol=TCP localport=135
-
-# -------- ACTIVE DIRECTORY (DOMAIN CONTROLLER PORTS) --------
-# Kerberos
-netsh advfirewall firewall add rule name="Kerberos-TCP" dir=in action=allow protocol=TCP localport=88
-netsh advfirewall firewall add rule name="Kerberos-UDP" dir=in action=allow protocol=UDP localport=88
-
-# LDAP
-netsh advfirewall firewall add rule name="LDAP-TCP" dir=in action=allow protocol=TCP localport=389
-netsh advfirewall firewall add rule name="LDAP-UDP" dir=in action=allow protocol=UDP localport=389
-
-# LDAPS
-netsh advfirewall firewall add rule name="LDAPS" dir=in action=allow protocol=TCP localport=636
-
-# Global Catalog
-netsh advfirewall firewall add rule name="GC" dir=in action=allow protocol=TCP localport=3268
-netsh advfirewall firewall add rule name="GC-SSL" dir=in action=allow protocol=TCP localport=3269
-
-# -------- DHCP SERVER --------
-netsh advfirewall firewall add rule name="DHCP-67" dir=in action=allow protocol=UDP localport=67
-netsh advfirewall firewall add rule name="DHCP-68" dir=in action=allow protocol=UDP localport=68
-
-# ============================================================
-# END RULESET
-# ============================================================
